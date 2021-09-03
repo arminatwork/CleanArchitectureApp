227d46f1ee4b7477ae7b28effd390d4f2f158f08
arminatwork <workwitharmin7@gmail.com>
2021-09-04 01:00:20 +0430

    add base auditable entity

---
+++ Server/src/CA.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CA.Domain.Common
+{
+    public abstract class AuditableEntity
+    {
+        public DateTime CreateDate { get; set; }
+        public DateTime? LastModifyDate { get; set; }
+        public string CreateBy { get; set; }
+        public string LastModifyBy { get; set; }
+    }
+}
