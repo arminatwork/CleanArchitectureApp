af4eb3d3aee5b55bb68d201a4e60bc1484b147fd
arminatwork <workwitharmin7@gmail.com>
2021-09-04 01:00:53 +0430

    add todo item,todo list entity with enum

---
+++ Server/src/CA.Domain/Entities/TodoList.cs
@@ -0,0 +1,13 @@
+﻿using CA.Domain.Common;
+using CA.Domain.ValueObjects;
+using System;
+
+namespace CA.Domain.Entities
+{
+    public class TodoList : AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public Color Color { get; set; } = Color.White;
+    }
+}
---
+++ Server/src/CA.Domain/Enums/PriorityLevel.cs
@@ -0,0 +1,10 @@
+﻿namespace CA.Domain.Enums
+{
+    public enum PriorityLevel
+    {
+        None,
+        Low,
+        Medium,
+        High
+    }
+}
