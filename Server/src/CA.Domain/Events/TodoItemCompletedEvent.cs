e30eb09fbf26658cc04cafc2064149d12be9bc19
arminatwork <workwitharmin7@gmail.com>
2021-09-04 00:58:53 +0430

    add todo item completed event

---
+++ Server/src/CA.Domain/Events/TodoItemCompletedEvent.cs
@@ -0,0 +1,15 @@
+﻿using CA.Domain.Common;
+using CA.Domain.Entities;
+
+namespace CA.Domain.Events
+{
+    public class TodoItemCompletedEvent : DomainEvent
+    {
+        public TodoItemCompletedEvent(TodoItem item)
+        {
+            Item = item;
+        }
+
+        public TodoItem Item { get; }
+    }
+}
