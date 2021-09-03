0cd27351fd7de3172d06b8cd7a204d16f8812eb5
arminatwork <workwitharmin7@gmail.com>
2021-09-04 00:56:33 +0430

    implement app db context

---
+++ Server/src/CA.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CA.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ Server/src/CA.Infrastructure/ServiceCollectionExtension.cs
@@ -0,0 +1,19 @@
+﻿using CA.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CA.Infrastructure
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void LoadDbContext(this IServiceCollection service, string connectionString)
+        {
+            var assembly = typeof(AppDbContext).Assembly.FullName;
+
+            service.AddDbContextPool<AppDbContext>(_ =>
+            {
+                _.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(assembly));
+            });
+        }
+    }
+}
