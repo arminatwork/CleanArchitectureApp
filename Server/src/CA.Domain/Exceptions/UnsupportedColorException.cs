78a6697f66a7ebd7927a51fdad5c616b9e39b0ea
arminatwork <workwitharmin7@gmail.com>
2021-09-04 00:57:51 +0430

    add unsupported color exception

---
+++ Server/src/CA.Domain/Exceptions/UnsupportedColorException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CA.Domain.Exceptions
+{
+    public class UnsupportedColorException : Exception
+    {
+        public UnsupportedColorException(string code) : base($"Color \"{code}\" is unsupported.")
+        {
+        }
+    }
+}
