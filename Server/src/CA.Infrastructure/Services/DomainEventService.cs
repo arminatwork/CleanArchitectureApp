b3a980aaa82c71fa7d78959d7329bfa17cd97b03
arminatwork <workwitharmin7@gmail.com>
2021-09-10 18:54:09 +0430

    implement domain event service

---
+++ Server/src/CA.Infrastructure/Services/DomainEventService.cs
@@ -0,0 +1,32 @@
+﻿using CA.Application.Common.Interfaces;
+using CA.Application.Common.Models;
+using CA.Domain.Common;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace CA.Infrastructure.Services
+{
+    public class DomainEventService : IDomainEventService
+    {
+        private readonly ILogger<DomainEventService> _logger;
+        private readonly IPublisher _publisher;
+
+        public DomainEventService(ILogger<DomainEventService> logger, IPublisher publisher)
+        {
+            _logger = logger;
+            _publisher = publisher;
+        }
+        public async Task Publish(DomainEvent domainEvent)
+        {
+            _logger.LogInformation("Published domain event. Event : {event}", domainEvent.GetType().Name);
+            await _publisher.Publish(this.GetNotificationCorrespondingToDomainEvent(domainEvent));
+        }
+
+        private INotification GetNotificationCorrespondingToDomainEvent(DomainEvent domainEvent)
+        {
+            return (INotification)Activator.CreateInstance(typeof(DomainEventNotification<>).MakeGenericType(domainEvent.GetType()), domainEvent);
+        }
+    }
+}
