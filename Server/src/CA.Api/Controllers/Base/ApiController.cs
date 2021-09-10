cccd7b72792ccafcccddc0c483252688f312d36b
arminatwork <workwitharmin7@gmail.com>
2021-09-10 19:51:16 +0430

    add base api controller

---
+++ Server/src/CA.Api/Controllers/Base/ApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CA.Api.Controllers.Base
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+    }
+}
