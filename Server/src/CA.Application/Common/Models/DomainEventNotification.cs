bda15feb168c6d55d10a8d05aa69ca9a0c2f3397
arminatwork <workwitharmin7@gmail.com>
2021-09-10 18:53:54 +0430

    add domain event notification model

---
+++ Server/src/CA.Application/Common/Models/DomainEventNotification.cs
@@ -0,0 +1,15 @@
+﻿using CA.Domain.Common;
+using MediatR;
+
+namespace CA.Application.Common.Models
+{
+    public class DomainEventNotification<TDomainEvent> : INotification where TDomainEvent : DomainEvent
+    {
+        public DomainEventNotification(TDomainEvent domainEvent)
+        {
+            DomainEvent = domainEvent;
+        }
+
+        public TDomainEvent DomainEvent { get; }
+    }
+}
