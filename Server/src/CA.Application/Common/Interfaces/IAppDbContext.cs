79204d0a3f2099920cb649f9afdba9f6f8ea4f3f
arminatwork <workwitharmin7@gmail.com>
2021-09-10 18:25:36 +0430

    add abstraction for appdbcontext

---
+++ Server/src/CA.Application/Common/Interfaces/IAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CA.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CA.Application.Common.Interfaces
+{
+    public interface IAppDbContext
+    {
+        DbSet<TodoList> TodoLists { get; set; }
+
+        DbSet<TodoItem> TodoItems { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
