0f523de395937de76a123e67e589a1001ed4bbd9
arminatwork <workwitharmin7@gmail.com>
2021-09-10 18:25:52 +0430

    fix domain model relationship

--- Server/src/CA.Domain/Entities/TodoList.cs
+++ Server/src/CA.Domain/Entities/TodoList.cs
@@ -1,6 +1,7 @@
 ﻿using CA.Domain.Common;
 using CA.Domain.ValueObjects;
 using System;
+using System.Collections.Generic;
 
 namespace CA.Domain.Entities
 {
@@ -9,5 +10,7 @@ namespace CA.Domain.Entities
         public Guid Id { get; set; }
         public string Title { get; set; }
         public Color Color { get; set; } = Color.White;
+
+        public IList<TodoItem> Items { get; private set; } = new List<TodoItem>();
     }
 }
