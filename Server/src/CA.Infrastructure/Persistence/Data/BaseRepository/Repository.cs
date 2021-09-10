21bf2558bf6fe8d29f65499ff12da930d8417d63
arminatwork <workwitharmin7@gmail.com>
2021-09-10 18:29:57 +0430

    add base repository for command

---
+++ Server/src/CA.Infrastructure/Persistence/Data/BaseRepository/Repository.cs
@@ -0,0 +1,37 @@
+﻿using CA.Domain.Common;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CA.Infrastructure.Persistence.Data.BaseRepository
+{
+    public class Repository<TEntity> : QueryRepository<TEntity>, IRepository<TEntity> where TEntity : AuditableEntity
+    {
+
+        private readonly AppDbContext _context;
+
+        public Repository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public virtual void Add(TEntity entity)
+        {
+            Entities.Add(entity);
+        }
+
+        public virtual async Task AddAsync(TEntity entity, CancellationToken cancellationToken)
+        {
+            await Entities.AddAsync(entity, cancellationToken);
+        }
+
+        public virtual void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public virtual async Task SaveAsync(CancellationToken cancellationToken)
+        {
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
